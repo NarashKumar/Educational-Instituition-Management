f58e4f36480810e798767cb67950d570138fb0f0
NarashKumar <narashworks@gmail.com>
2024-06-08 21:11:04 +0530

    add

--- FrontEnd/educational-institution-management/src/layouts/SidebarLayout.jsx
+++ FrontEnd/educational-institution-management/src/layouts/SidebarLayout.jsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar';
 const sidebarItems = [
     { label: 'Dashboard', path: '/dashboard' },
     {label: 'Personal Details', path: '/personal-details'},
-    { label: 'Fee Payment', path: '/fee-payment' },
+    { label: 'Fee Details', path: '/fee-payment' },
     { label: 'Course List', path: '/course-list' },
     { label: 'Grade / Mark & Credit', path: '/grade-credit' },
     // Add other sidebar items as needed
